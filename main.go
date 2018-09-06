1cf55f62ae8ee34bb9fec64508398480b9168e2c
bonfy <foreverbonfy@163.com>
2018-09-06 22:32:18 +0800

    02-Template add simple text template

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
+
+// User struct
+type User struct {
+	Username string
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
+		user := User{Username: "bonfy"}
+		tpl, _ := template.New("").Parse(`<html>
+			<head>
+				<title>Home Page - Bonfy</title>
+			</head>
+			<body>
+				<h1>Hello, {{.Username}}!</h1>
+			</body>
+		</html>`)
+		tpl.Execute(w, &user)
 	})
 	http.ListenAndServe(":8888", nil)
 }
