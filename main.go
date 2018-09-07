cca162aa6510f6d8bf96cb21ef8aed5ea0a1a1a1
bonfy <foreverbonfy@163.com>
2018-09-07 10:45:57 +0800

    02-Template add templates folder

--- main.go
+++ main.go
@@ -13,14 +13,7 @@ type User struct {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		user := User{Username: "bonfy"}
-		tpl, _ := template.New("").Parse(`<html>
-			<head>
-				<title>Home Page - Bonfy</title>
-			</head>
-			<body>
-				<h1>Hello, {{.Username}}!</h1>
-			</body>
-		</html>`)
+		tpl, _ := template.ParseFiles("templates/index.html")
 		tpl.Execute(w, &user)
 	})
 	http.ListenAndServe(":8888", nil)
