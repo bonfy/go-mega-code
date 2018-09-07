9df5fb2f1f7d400513e3877aad0704a5e69366ac
bonfy <foreverbonfy@163.com>
2018-09-07 11:12:51 +0800

    02-Template if-else

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ type User struct {
 	Username string
 }
 
+// IndexViewModel struct
+type IndexViewModel struct {
+	Title string
+	User  User
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		user := User{Username: "bonfy"}
+		v := IndexViewModel{Title: "Homepage", User: user}
 		tpl, _ := template.ParseFiles("templates/index.html")
-		tpl.Execute(w, &user)
+		tpl.Execute(w, &v)
 	})
 	http.ListenAndServe(":8888", nil)
 }
