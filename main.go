50e5028870d4ab7918d391007162c4387540f165
bonfy <foreverbonfy@163.com>
2018-09-07 13:39:18 +0800

    02-Template range

--- main.go
+++ main.go
@@ -10,16 +10,30 @@ type User struct {
 	Username string
 }
 
+// Post struct
+type Post struct {
+	User User
+	Body string
+}
+
 // IndexViewModel struct
 type IndexViewModel struct {
 	Title string
 	User  User
+	Posts []Post
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		user := User{Username: "bonfy"}
-		v := IndexViewModel{Title: "Homepage", User: user}
+		u1 := User{Username: "bonfy"}
+		u2 := User{Username: "rene"}
+
+		posts := []Post{
+			Post{User: u1, Body: "Beautiful day in Portland!"},
+			Post{User: u2, Body: "The Avengers movie was so cool!"},
+		}
+
+		v := IndexViewModel{Title: "Homepage", User: u1, Posts: posts}
 		tpl, _ := template.ParseFiles("templates/index.html")
 		tpl.Execute(w, &v)
 	})
